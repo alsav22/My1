38962da472468f9d999d04ee523756d100282b9f
alsav22 <alsav22@rambler.ru>
2016-04-12 14:15:06 +0300

    begin

---
+++ MyClass.cpp
@@ -0,0 +1,11 @@
+#include "MyClass.h"
+#include <iostream>
+
+MyClass::~MyClass()
+{
+}
+
+void MyClass::show()
+{
+	std::cout << "Done!" << std::endl;
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "MyClass.h"
+#include <iostream>
+
+int main()
+{
+    MyClass obj(5);
+	obj.show();
+	MyClass obj1(1);
+	MyClass obj2(2);
+	MyClass obj3(3);
+	std::cout << "Done!" << std::endl;
+	
+	system("pause");
+	return 0;
+}
